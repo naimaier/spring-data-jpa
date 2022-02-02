fd48b40086d3bcb3916bf4679c342b8feb0e2d4c
Henrique Naimaier <coubron@yahoo.com.br>
2022-02-02 11:36:27 -0300

    First repository

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,13 +1,33 @@
 package br.com.alura.spring.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringDataApplication {
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
 
+//CommandLineRunner gera um método run que roda após o início da aplicação
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner{
+
+	private final CargoRepository repository;
+	
+	//Injetando a dependência
+	public SpringDataApplication(CargoRepository repository) {
+		this.repository = repository;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		
+		repository.save(cargo);
 	}
 
 }
